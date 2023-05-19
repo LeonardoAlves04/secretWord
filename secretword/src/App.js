0ae9cc5e8a3621fbc0e5932f1476896853f4fb15
Leonardo Alves <leonardoalves201510@gmail.com>
2023-05-19 14:36:10 -0300

    feat: iniciando projeto e criando componentes

---
+++ secretword/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import StartScreen from './components/StartScreen';
+import { useCallBack, useEffect, useState } from "react";
+import { wordsList } from "./data/words"
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
+
+function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList)
+
+  console.log(words)
+
+  return (
+    <div className="App">
+      {gameStage === "start" && <StartScreen />}
+      {gameStage === "game" && <Game />}
+      {gameStage === "end" && <GameOver />}
+    </div>
+  );
+}
+
+export default App;
---
+++ secretword/src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import "./StartScreen.css"
+
+const StartScreen = () => {
+    return (
+        <div className='start'>
+            <h1>Secret Word</h1>
+            <p>Clique aqui para começar a jogar:</p>
+            <button>começar o jogo</button>
+        </div>
+    )
+}
+
+export default StartScreen
