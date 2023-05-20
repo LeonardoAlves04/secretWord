ad1d12c94efefad87ff88cca972eaf1a251b3b86
Leonardo Alves <leonardoalves201510@gmail.com>
2023-05-20 15:51:50 -0300

    feat: settando palavra/categoria de forma aleatoria

--- secretword/src/App.js
+++ secretword/src/App.js
@@ -14,8 +14,14 @@ const stages = [
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList)
+  const [pickedWord, setPickedWord] = useState("")
+  const [pickedCategory, setPickedCategory] = useState("")
+  const [letters, setLetters] = useState([])
 
+  const pickWordAndCategory = () => { }
   const startGame = () => {
+
+
     setGameStage(stages[1].name)
   }
 
