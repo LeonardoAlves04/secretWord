5e78c0f96662796878b6d19e65436dd39f4a426d
Leonardo Alves <leonardoalves201510@gmail.com>
2023-05-23 10:53:22 -0300

    feat: escolhendo palavra/categoria de forma aleatória

--- secretword/src/App.js
+++ secretword/src/App.js
@@ -6,6 +6,8 @@ import Game from './components/Game';
 import GameOver from './components/GameOver';
 
 const stages = [
+  // estágios do jogo. Tela inicial, jogo e fim do jogo
+
   { id: 1, name: "start" },
   { id: 2, name: "game" },
   { id: 3, name: "end" },
@@ -19,13 +21,35 @@ function App() {
   const [letters, setLetters] = useState([])
 
   const pickWordAndCategory = () => {
+    // escolhendo uma categoria aleatória
+    const categories = Object.keys(words);
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
+    console.log(category)
 
+    // escolhendo uma palavra aleatória
+    const word = words[category][Math.floor(Math.random() * words[category].length)];
+    console.log(word);
+
+    return { category, word }
   }
 
   const startGame = () => {
-
-
     setGameStage(stages[1].name)
+
+    // desestruturando a função acima para escolher uma palavra e categoria aleatoria
+    const { category, word } = pickWordAndCategory();
+
+    // criando um array de letras
+    let wordLetters = word.split("");
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    // settando os estados
+    setPickedWord(word)
+    setPickedCategory(category)
+    setLetters(letters)
+
+    console.log(wordLetters)
+    console.log(category, word)
   }
 
   const verifyLetter = () => {
